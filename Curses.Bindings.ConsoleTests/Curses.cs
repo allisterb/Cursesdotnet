ab865785264fb4fac465b186a7623f9416055319
Allister Beharry <allister.beharry@gmail.com>
2016-03-03 00:42:07 -0400

    Added missing files.

---
+++ Curses.Bindings.ConsoleTests/Curses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curses.Bindings.ConsoleTests
+{
+    /// <summary>
+    /// High-level .NET interface to the curses/pcurses library bindings.
+    /// </summary>
+    public class Curses
+    {
+        public static WINDOW StdScr { get; private set; }
+        
+        public static void InitScr()
+        {
+            StdScr = _curses.initscr();
+        } 
+
+        public static void PrintW(string s)
+        {
+            _curses.printw(s);
+        }
+    }
+}
